a5e553f42a51ab056c05f518d5a672beb407582a
rafa <rafael@queo.pt>
2012-11-15 11:20:14 +0000

    import do admin

--- project-base/project_name/urls.py
+++ project-base/project_name/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
