04c8773a0d577bed66836621d0eca89c3eff3385
rafa <rafael@queo.pt>
2012-12-06 14:06:55 +0000

    adicionado o ficheiro de utils de javascript

---
+++ project-base/project_name/static/js/queoutils.js
@@ -0,0 +1,37 @@
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
+function arraySize(myArray) {
+    var size = 0, key;
+    for (key in myArray) {
+        if (myArray.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+function toArray(strg){
+    strg = strg.replace(/left|top/g,'0px');
+    strg = strg.replace(/right|bottom/g,'100%');
+    strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2");
+    var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
+    return [parseFloat(res[1],10),res[2],parseFloat(res[3],10),res[4]];
+}
+
+var slug = function(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "ãàáäâéèéëêìíïîõòóöôùúüûñç·/_,:;";
+  var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+};
